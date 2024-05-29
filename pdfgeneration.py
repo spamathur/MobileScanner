32575fde2acdbec49722b32ad688345d7612efdc
Sparsh Mathur <107881186+spamathur@users.noreply.github.com>
2024-05-29 17:44:28 -0500

    Insert images

--- pdfgeneration.py
+++ pdfgeneration.py
@@ -1,18 +1,18 @@
 from reportlab.lib.pagesizes import letter # Generating PDF
 from reportlab.pdfgen import canvas # Generating PDF
+from io import BytesIO # Transferring image data
 
-def create_pdf(name, image):
+def create_pdf(name, image_data):
     c = canvas.Canvas(name, pagesize=letter)
     width, height = letter
 
     # Add title
     c.setFont("Helvetica-Bold", 24)
-    c.drawString(100, height - 100, "Hello, PDF World!")
+    c.drawString(100, height - 100, "Here is your scanned pdf!")
 
-    # Add some text
-    c.setFont("Helvetica", 12)
-    text = "This is a simple PDF file created using the reportlab library in Python."
-    c.drawString(100, height - 150, text)
+    # Add the scanned image
+    image = BytesIO(image_data)
+    c.drawImage(image, 100, height - 300, width=200, height=100) 
 
     # Save the PDF file
     c.save()
